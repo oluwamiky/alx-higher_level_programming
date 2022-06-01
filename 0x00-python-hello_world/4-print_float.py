99558a1d65103dd089ab6362ffe402842e8f72da
oluwamiky <jnguessan22@yahoo.com>
2022-06-01 16:52:59 +0000

    print float variable number

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 3.14159
+print(f"{'float:'} {number:.2f}")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[F[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
