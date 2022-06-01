76e16b96f822c8303398231d6889751f3aa9c016
oluwamiky <jnguessan22@yahoo.com>
2022-06-01 14:11:31 +0000

    print text

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+!#/usr/bin/python3[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[[C[C![C[C[C[C[C[C[C[C[C#!/usr/bin/python3
+print("[1;2o[1;("\"Programming is like building a multilingual puzzle")
